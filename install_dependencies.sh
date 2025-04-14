ef4478521271cc6b5e54aa96bc5e24d979eb3ce8
ColinBalfour <75389927+ColinBalfour@users.noreply.github.com>
2025-04-14 16:13:37 -0400

    updated install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,9 +5,24 @@ git clone https://github.com/apple/ml-depth-pro
 cd ml-depth-pro
 pip install -e .
 source get_pretrained_models.sh
+cd ..
+
+# Detic dependency
+git clone git@github.com:facebookresearch/detectron2.git
+cd detectron2
+pip install -e .
+
+cd ..
+git clone https://github.com/facebookresearch/Detic.git --recurse-submodules
+cd Detic
+pip install -r requirements.txt
 
 # python dependencies
 pip install opencv-python
 pip install opencv-python-headless
 pip install scipy
 pip install ultralytics
+
+
+# Data install (todo)
+curl -L -o P3Data.zip "https://app.box.com/shared/static/zjys9xcefyqfj2oxkwsm5irgz6g3hp1l.zip"
